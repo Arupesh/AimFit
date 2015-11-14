159ca157f5b3d6adc8a41bbf1c1418ec3b1aa5b4
Arupesh RoyChowdhury <arupesh.roychowdhury1988@gmail.com>
2015-11-15 02:50:18 +0530

    chat plugin update :- name : message implementation

--- source/public/controllers/chatCtrl.js
+++ source/public/controllers/chatCtrl.js
@@ -1,8 +1,9 @@
 myApp.controller('chatCtrl',["$scope","socket",function($scope,socket){
  	  $scope.msgs=[];
+
     var person = prompt("Please enter your name", "Harry Potter");
     $scope.sendMsg=function(){
-       socket.emit('send msg', $scope.msg.text)
+       socket.emit('send msg', {name : person , msg : $scope.msg.text} )
        $scope.msg='';
     }
     socket.on('get msg' , function(data){
