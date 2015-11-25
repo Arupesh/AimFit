274dbe3e3e0030232274ccef2072e376985c2331
Arupesh RoyChowdhury <arupesh.roychowdhury1988@gmail.com>
2015-11-25 13:04:35 +0530

    mongodb connection implementation

--- source/public/controllers/controller.js
+++ source/public/controllers/controller.js
@@ -3,14 +3,14 @@
 //var myApp = angular.module('myApps',[]);
 
 
-myApp.controller('AppCtrl',['$scope', '$http',function ($scope,$http)
+myApp.controller('AppCtrl',['$scope', '$http',"$location",function ($scope,$http,$location)
 {
     
 console.log("inside controller"); 
  
 var refresh=function(){
         $http.get('/contactList').success(function(response){
-        console.log("got the data");                            
+        console.log("got the data",response);                            
         $scope.contactList=response;
              });
         $scope.contact="";
@@ -19,39 +19,40 @@ var refresh=function(){
 refresh();
 
 //Add contact function
-$scope.addContact=function(){
-    console.log("Data to be posted >>"+JSON.stringify($scope.contact))
- $http.post('/contactList',$scope.contact).success(function(response){
+$scope.addContact=function(userData){
+   alert("Data to be posted >>"+JSON.stringify(userData))
+ $http.post('/contactList',userData).success(function(response){
  console.log(JSON.stringify(response));
+ $location.path('/about');
  refresh();
  });
 };
 
 //Remove contact function    
-$scope.remove=function(id){
-    console.log(id);
-        $http.delete('/contactList/'+id).success(function(response){
-        refresh();
-        });
-    };
+// $scope.remove=function(id){
+//     console.log(id);
+//         $http.delete('/contactList/'+id).success(function(response){
+//         refresh();
+//         });
+//     };
                             
                             
-$scope.edit=function(id)
-   {
- console.log(id);
-            $http.get('/contactList/'+id).success(function(response){
-       $scope.contact=response;
-       });
-   };
+// $scope.edit=function(id)
+//    {
+//  console.log(id);
+//             $http.get('/contactList/'+id).success(function(response){
+//        $scope.contact=response;
+//        });
+//    };
     
-    $scope.update=function()
-   {
- console.log($scope.contact._id);
-            $http.put('/contactList/'+$scope.contact._id,$scope.contact).success(function(response){
-       refresh();
-       });
+//     $scope.update=function()
+//    {
+//  console.log($scope.contact._id);
+//             $http.put('/contactList/'+$scope.contact._id,$scope.contact).success(function(response){
+//        refresh();
+//        });
     
-   };
+//    };
 
 
 }]);
--- source/server.js
+++ source/server.js
@@ -5,10 +5,12 @@ var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://55.55.55.5/mongo');
 
+var mongojs= require('mongojs');
+var db =mongojs('contactlist',['contactlist']);
 
-//var mongojs= require('mongojs');
-//var db =mongojs('contactlist',['contactlist']);
 var passport      = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 app.use(passport.initialize());
@@ -40,43 +42,48 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-     console.log("LocalStrategy");
-  if(username==password)
-      return done(null, {username:username, firstname: 'abhilasha'}); 
-  else
-     return done(null, false, {message:'Unable to login'}); 
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//      console.log("LocalStrategy");
+//   if(username==password)
+//       return done(null, {username:username, firstname: 'abhilasha'}); 
+//   else
+//      return done(null, false, {message:'Unable to login'}); 
 
-  }
-)); 
+//   }
+// )); 
 
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
 
-app.post('/login',
-  passport.authenticate('local'),function(req,res){
-        console.log("req.body /login"+req.user);
-        res.json(req.user);
+// app.post('/login',
+//   passport.authenticate('local'),function(req,res){
+//         console.log("req.body /login"+req.user);
+//         res.json(req.user);
 
-});
+// });
 
 
 app.get('/contactList',function(req, res){
+  console.log("servive call made");
 
  db.contactlist.find(function(err, users) {
   if( err || !users) 
     console.log("No data user found");
   else 
+  {
     res.json(users);
+    console.log("Data>>>",users)
+  }
+    
 });
 });
     
@@ -85,41 +92,41 @@ app.get('/contactList',function(req, res){
 app.post('/contactList',function(req,res){
         console.log("req.body"+JSON.stringify(req.body));
         db.contactlist.insert(req.body,function(err,doc){
-    res.json(doc);
+        res.json(doc);
         });
 
 });
     
-app.delete('/contactList/:id',function(req,res){
-    console.log("called")
-    var id=req.params.id;
-    console.log("id to be deleted"+id);
-    db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
-    res.json(doc);
-    });
+// app.delete('/contactList/:id',function(req,res){
+//     console.log("called")
+//     var id=req.params.id;
+//     console.log("id to be deleted"+id);
+//     db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+//     res.json(doc);
+//     });
     
-});
+// });
     
-app.get('/contactList/:id',function(req,res){
-    var id=req.params.id;
-     db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
-    res.json(doc);
-        });
+// app.get('/contactList/:id',function(req,res){
+//     var id=req.params.id;
+//      db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+//     res.json(doc);
+//         });
 
-});
+// });
 
 
-app.put('/contactList/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(req.body.name);
-  db.contactlist.findAndModify({
-    query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
-    new: true}, function (err, doc) {
-      res.json(doc);
-    }
-  );
-});
+// app.put('/contactList/:id', function (req, res) {
+//   var id = req.params.id;
+//   console.log(req.body.name);
+//   db.contactlist.findAndModify({
+//     query: {_id: mongojs.ObjectId(id)},
+//     update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+//     new: true}, function (err, doc) {
+//       res.json(doc);
+//     }
+//   );
+// });
  
     
 server.listen(3000);
